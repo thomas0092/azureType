81f68f7a1ad243a9e33cf0941c84f991b64fe78a
thomas0092 <thomas.severgnini@refactormi.com>
2024-08-07 10:33:01 +0200

    Add files via upload

---
+++ azure-ex-collab-fixing-bugs/src/utils/filters.ts
@@ -0,0 +1,36 @@
+import { Pokemon, Location, Move } from './pokemonTypes';
+
+
+export function filterPokemon(pokemon: Pokemon) {
+    return {
+        id: pokemon.id,
+        name: pokemon.name,
+        height: pokemon.height,
+        weight: pokemon.weight,
+        baseExperience: pokemon.baseExperience,
+        species: {
+            name: pokemon.species.name,
+            url: pokemon.species.url
+        }
+    };
+}
+
+
+export function filterLocation(location: Location) {
+    return {
+        id: location.id,
+        name: location.name,
+    }
+}
+
+export function filterMove(move: Move) {
+    return {
+        id: move.id,
+        name: move.name,
+        accuracy: move.accuracy,
+        effect_chance: move.effect_chance,
+        pp: move.pp,
+        priority: move.priority,
+        power: move.power,
+    }
+}
